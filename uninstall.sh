79038246c7e2b969a6ecbfa3e4555912dc5a7b71
Wartybix <34974060+Wartybix@users.noreply.github.com>
2023-12-28 14:03:46 +0000

    Added uninstaller

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+echo "Removing program..."
+sudo rm /usr/local/bin/huawei-soundcard-headphones-monitor.sh
+
+echo "Removing service..."
+sudo rm /etc/systemd/system/huawei-soundcard-headphones-monitor.service
+
+echo "Uninstalled. Goodbye 😿"
